1b156a8870302c3a4a9d7a20231f211eaf5ee4e8
salman354 <mskhn2255@gmail.com>
2022-11-10 12:44:30 +0530

    change in app file

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template
 import pandas as pd
 import numpy as np
-
 app=Flask(__name__)
 #load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
